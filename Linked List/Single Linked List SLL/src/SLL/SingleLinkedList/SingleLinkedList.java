1c82b793c776a3884c00848868624f477adaf0cb
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-11-27 23:52:27 +0200

    Update SingleLinkedList.java

--- Linked List/Single Linked List SLL/src/SLL/SingleLinkedList/SingleLinkedList.java
+++ Linked List/Single Linked List SLL/src/SLL/SingleLinkedList/SingleLinkedList.java
@@ -4,11 +4,25 @@ import SLL.*;
 public class SingleLinkedList {
     private Node head;
     private Node tail;
+    private int length;
 
     //----------------------------------------------------------------
 
     public SingleLinkedList() {
         this.head = this.tail = null;
+        this.length = 0;
+    }
+
+    //----------------------------------------------------------------
+
+    public int length(){
+        return this.length;
+    }
+
+    //----------------------------------------------------------------
+
+    public boolean empty(){
+        return this.head == null;
     }
 
     //----------------------------------------------------------------
@@ -17,15 +31,51 @@ public class SingleLinkedList {
 
         Node newNode = new Node(data);
 
-        if (head == null){
+        if (empty()){
             this.head = this.tail = newNode;
         }
         else{
             this.tail.next = newNode;
             this.tail = newNode;
         }
+        length++;
     }
 
+    //----------------------------------------------------------------
+
+    public void insertAtHead(int data){
+
+        Node newNode = new Node(data);
+
+        if (empty()){
+            this.head = this.tail = newNode;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+
+    //----------------------------------------------------------------
+
+    public void insertAtIndex(int data, int index){
+        if (length < index || index < 0){
+            System.out.println("Index out of bounds");
+        return;
+        }
+
+        int counter = 0;
+        Node newNode = new Node(data);
+        Node current = this.head;
+
+        while (counter <= index){
+            current = current.next;
+            counter++;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+    
     //----------------------------------------------------------------
 
     public void print (){
