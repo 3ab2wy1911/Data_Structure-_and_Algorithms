de20881724bdf5bf1cb14c25faf7c23b5326507e
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-11-28 07:49:36 +0200

    Testing LinkedList

--- Linked List/Single Linked List SLL/src/Main.java
+++ Linked List/Single Linked List SLL/src/Main.java
@@ -9,19 +9,21 @@ public class Main {
         list.insert(2); // [1, 2]
         list.insert(3); // [1, 2, 3]
         list.insertAtHead(0);   // [0, 1, 2, 3]
+        list.insertAtIndex(7,2);    // [0, 1, 7, 2, 3]
 
         // Traverse, Search & Length Testing
-        list.traverse(); // [0, 1, 2, 3]
-        System.out.println("\nLength is : " + list.length());   // Length is : 4
-        System.out.println("Index of element 2 : " + list.search(2));   // Index of element 2 : 2
+        list.traverse(); // [0, 1, 7, 2, 3]
+        System.out.println("\nLength is : " + list.length());   // Length is : 5
+        System.out.println("Index of element 2 : " + list.search(2));   // Index of element 2 : 3
 
         // Delete Testing
-        list.deleteAtIndex(1);  // [0,2,3]
-        list.deleteTail();  // [0,2]
+        list.deleteAtIndex(1);  // [0, 7, 2, 3]
+        list.deleteTail();  // [0, 7, 2]
+        list.deleteHead();  // [7, 2]
         list.deleteHead();  // [2]
         list.traverse(); // [2]
         System.out.println("\nLength is : " + list.length());   // Length is : 1
-        System.out.println("Index of element 2 : " + list.search(1));   // Index of element 1 : -1
+        System.out.println("Index of element 1 : " + list.search(1));   // Index of element 1 : -1
 
         // Clear & Empty testing
         list.clear();   // []
