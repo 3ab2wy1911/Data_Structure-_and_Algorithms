e96fca79c5b69958b6876ac12624623cb6f37b41
3ab2wy1911 <3ab2wy1911@gmail.com>
2024-02-08 20:19:14 +0200

    122. Best Time to Buy and Sell Stock II

---
+++ Array/Problems/src/Best_Time_To_Buy_And_Sell_Stock_II/Solution.java
@@ -0,0 +1,13 @@
+package Best_Time_To_Buy_And_Sell_Stock_II;
+// LeetCode Problem : 122. Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i =1 ; i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
